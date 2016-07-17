2c9789ef3e42902064af29f08859b2eb523998c9
Elizabeth Nelson <elizabeth.larkin.nelson@gmail.com>
2016-07-17 19:16:49 +0000

    First part of assessment complete.

---
+++ assessment.py
@@ -0,0 +1,60 @@
+"""
+Part 1: Discussion
+
+1. What are the three main design advantages that object orientation
+   can provide? Explain each concept.
+
+The three main design advantages that object orientation provides are:
+1) Encapsulation:
+    This allows data to live closer to its functionality.  Another way to
+    explain that would be to say the data and the functions that manipulate
+    the data are kept safe from outside interference.  This allows the 
+    values or state of a structured data object inside a class to be hidden.
+
+2) Abstraction:
+    In abstraction a method can be utilized without knowing the information
+    that method uses internally.  This allows the programmer to hide all but
+    the relevant data about an object so complexity is reduced and efficiency
+    is increased.
+
+3) Inheritance:
+    This allows for different and interchangeable types.  The benefit of
+    inheritance is specifiying implementation of same behavior and allows
+    for code reuse.  Inheritance can also be used in a single or multiple
+    fashion allowing for hierarchical and hybrid inhertance types.
+
+2. What is a class?
+    A class is a type of thing.  This is a broad definition for the methods 
+    and variables that are particular to a kind of object.  The object is
+    a specific instance of a class; meaning the class contains real values
+    instead of variables.
+
+3. What is an instance attribute?
+    An instance attribute is a specific instance of a class 
+    containing real values.
+
+4. What is a method?
+    A method is a function (a set of instructions, procedure) defined 
+    on a class.
+
+5. What is an instance in object orientation?
+    An instance is an individual instance of a class.  This means that
+    there is creation of a realized instance (instantiation).  The
+    instance attributes are realized upon instantiation.
+
+6. How is a class attribute different than an instance attribute?
+   Give an example of when you might use each.
+   A class attribute is inherent to that class and does not change upon
+   instantiation unless otherwise specified.  An instance attribute is
+   an attribute that is given upon instantiation of the object.
+
+   A class attribute would be utilized for a shared trait all potential
+   child classes may utilize or an attribute that would be static for all 
+   instantiations of that class.
+
+
+"""
+
+
+# Parts 2 through 5:
+# Create your classes and class methods
